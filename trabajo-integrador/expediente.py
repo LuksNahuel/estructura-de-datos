7c1cb9c337fa686f7820482f83e67ef49f90ba3a
LuksNahuel <melianlucasnahuel@gmail.com>
2019-10-07 17:25:03 -0300

    Avances sobre el ejercicio integrador

--- trabajo-integrador/expediente.py
+++ trabajo-integrador/expediente.py
@@ -6,6 +6,7 @@ Created on Fri Oct  4 20:23:28 2019
 @author: estruc-datos
 """
 import cola
+import numpy as np
 
 def esCausaValida(causa):
     return causa == "civil" or causa == "penal" or causa == "criminal"
@@ -35,6 +36,9 @@ class Expediente():
     def getPrioridad(self):
         return self.prioridad
     
+    def getTipo(self):
+        return self.tipoCausa
+        
 class Juzgado():
     def __init__(self, capacidad = 50):
         self.normales = cola.Cola(capacidad, dtype = Expediente)
@@ -43,7 +47,7 @@ class Juzgado():
     def recibirExpediente(self, exp):
         if exp.getPrioridad() == "normal":
             self.normales.queue(exp)
-        elif exp.getPrioridad() == "urgente":
+        else:
             self.urgentes.queue(exp)
     
     def primerExpedienteATratar(self):
@@ -53,3 +57,50 @@ class Juzgado():
         else:
             salida = self.normales.top()
         return salida
+    
+    def cantidadDeExpedientes(self):
+        return self.normales.getLen() + self.urgentes.getLen()
+    
+    def cantidadPorTipo(self, tipoExp):
+        auxNormales = self.normales.clone()
+        cantidadNormal = 0
+        while not auxNormales.isEmpty():
+            data = auxNormales.dequeue()
+            if data.getTipo() == tipoExp:
+                cantidadNormal += 1
+        
+        auxUrgentes = self.urgentes.clone()
+        cantidadUrgente = 0
+        while not auxUrgentes.isEmpty():
+            data = auxUrgentes.dequeue()
+            if data.getTipo() == tipoExp:
+                cantidadUrgente += 1
+        
+        return np.array([cantidadNormal, cantidadUrgente])
+    
+    
+    
+    
+    
+    
+    
+    
+def cantidadPorMeses(meses):
+    salida = 0
+    if meses <= 2:
+        salida = 250
+    elif meses % 2 == 0:
+        salida = cantidadPorMeses(meses - 1) + cantidadPorMeses(meses - 2)
+    else:
+        salida = cantidadPorMeses(meses - 1) * 3 + 20
+    return salida
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
